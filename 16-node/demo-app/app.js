a9ba878721282adb8345ff6b0447e305da9078b7
Traian <niculescu.traian.ct@gmail.com>
2020-03-01 23:06:14 +0200

    knock-knock joke app

---
+++ 16-node/demo-app/app.js
@@ -0,0 +1,29 @@
+let knockKnock = require('knock-knock-jokes');
+let rl = require('readline');
+const readline = require('readline');
+
+rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const askQuestion = () => {
+  rl.question('Do you wanna hear a knock-knock joke? y/n ', answer => {
+    if (answer === 'y') {
+      console.log(knockKnock());
+      askQuestion();
+    } else if (answer === 'n') {
+      rl.close();
+    } else {
+      console.log("Please input only 'y' or 'n'");
+      askQuestion();
+    }
+  });
+};
+
+rl.on('close', function() {
+  console.log('\nBYE-BYE');
+  process.exit(0);
+});
+
+askQuestion();
