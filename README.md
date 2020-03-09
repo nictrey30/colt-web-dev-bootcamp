21239b47071cb4e8b52cf8968108ef528677479c
Traian <niculescu.traian.ct@gmail.com>
2020-03-09 20:13:49 +0200

    yelp v2 -data persistence

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# colt-web-dev-bootcamp
+#YelpCamp
+
+- Add Landing Page
+- Add Campgrounds Page that lists all campgrounds
+
+Each Campground has:
+
+- Name
+- Image
+
+#Layout and basic styling
+
+- Create our header and footer partials
+- Add in Bootstrap
+
+# Creating New Campgrounds
+
+- Setup new campground POST route
+- Add in body-parser
+- Setup route to show form
+- Add basic unstyled form
+
+# Style the Navbar and Form
+
+- Add a navbar to all templates
+- Style the new campground form
+
+# Databases
+
+## Intro to databeses
+
+- What is a database?
+- Sql vs NoSql
+
+# Add Mongoose
+
+- Install and configure mongoose
+- Setup campground model
+- Use campground model inside of our routes!
+
+# RESTFUL ROUTES
+
+| **URL**          | **HTTP Verb** | **Action** |
+| ---------------- | ------------- | ---------- |
+| /photos/         | GET           | index      |
+| /photos/new      | GET           | new        |
+| /photos          | POST          | create     |
+| /photos/:id      | GET           | show       |
+| /photos/:id/edit | GET           | edit       |
+| /photos/:id      | PATCH/PUT     | update     |
+| /photos/:id      | DELETE        | destroy    |
+
+| **name** | **url**   | **Verb** | **desc**                      |
+| -------- | --------- | -------- | ----------------------------- |
+| index    | /dogs     | GET      | display a list of all dogs    |
+| new      | /dogs/new | GET      | the route that shows the form |
+| create   | /dogs     | POST     | add new dog to the database   |
+| show     | /dogs/:id | GET      | shows info about one dog      |
--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -1,63 +1,104 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
+
+const port = 3000;
+
+// deprecation handlers mongoose
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+
+// connect to a db
+mongoose.connect('mongodb://localhost:27017/yelp_camp');
 app.use(bodyParser.urlencoded({ extended: true }));
 // serve the contents of the 'public' folder
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-let campgrounds = [
+// schema setup
+let campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+let Campground = mongoose.model('Campground', campgroundSchema);
+
+// add a new campground to the database
+/*Campground.create(
   {
     name: 'Salmon Creek',
     image:
-      'https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg'
+      'https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg',
+    description:
+      " Sewer smells, run down crowded park on the poor side of town. Wifi was nonexistent most of the time. We won't return! "
   },
-  {
-    name: 'Granite Hill',
-    image:
-      'https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg'
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image:
-      'https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_960_720.jpg'
-  },
-  {
-    name: 'Salmon Creek',
-    image:
-      'https://cdn.pixabay.com/photo/2016/11/21/16/03/campfire-1846142_960_720.jpg'
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image:
-      'https://cdn.pixabay.com/photo/2016/11/29/09/09/abstract-1868624_960_720.jpg'
-  },
-  {
-    name: 'Granite Hill',
-    image:
-      'https://pixabay.com/get/57e5dd474c51b108f5d084609620367d1c3ed9e04e50744176297bd09449c1_340.png'
+  (err, campground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('new campground created!');
+      console.log(campground);
+    }
   }
-];
+);*/
 
 app.get('/', function(req, res) {
   res.render('landing');
 });
+
+// index - show all campgrounds
 app.get('/campgrounds', function(req, res) {
-  res.render('campgrounds', { campgrounds: campgrounds });
+  // get all the campgrounds from db
+  Campground.find({}, (err, allCampgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', { campgrounds: allCampgrounds });
+    }
+  });
 });
+
+// new - show form to create campground
 app.get('/campgrounds/new', function(req, res) {
   res.render('new');
 });
 
+// create - add new campground to db
 // same url, but totally different routes - follows REST
 app.post('/campgrounds', function(req, res) {
   // res.send('You hit the post route');
   // get data from form and add to campgrounds array
   let name = req.body.name;
   let image = req.body.image;
-  campgrounds.push({ name, image });
-  // redirect back to campgrounds page
-  res.redirect('/campgrounds');
+  let description = req.body.description;
+  // create a new campground and save to database
+  let newCampground = { name, image, description };
+  Campground.create(newCampground, (err, newlyCreated) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`campground created \n ${newlyCreated}`);
+      // redirect back to campgrounds page
+      res.redirect('/campgrounds');
+    }
+  });
 });
 
-app.listen(3000, () => console.log('YelpCamp Server has started!'));
+// show - shows more info about one campground
+app.get('/campgrounds/:id', function(req, res) {
+  // find the campground with the provided id
+  let id = req.params.id;
+  Campground.findById(id, (err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // render show template with that campground
+      res.render('show', { campground: foundCampground });
+    }
+  });
+});
+
+app.listen(port, () =>
+  console.log(`YelpCamp Server has started! on port ${port}`)
+);
