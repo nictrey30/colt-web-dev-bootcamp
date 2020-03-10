1ee07b0ace08a9563a2f5125e7adcd5d579f577f
Traian <niculescu.traian.ct@gmail.com>
2020-03-10 11:35:01 +0200

    update restful blog app

--- 20-RESTful-Routing/RESTfulBlogApp/app.js
+++ 20-RESTful-Routing/RESTfulBlogApp/app.js
@@ -22,12 +22,30 @@ let Blog = mongoose.model('Blog', blogSchema);
 app.get('/', function(req, res) {
   res.redirect('/blogs');
 });
+// INDEX ROUTE
 app.get('/blogs', function(req, res) {
   Blog.find({}, (err, blogs) => {
     if (err) {
       console.log(err);
     } else {
       res.render('index', { blogs: blogs });
+    }
+  });
+});
+// NEW ROUTE
+app.get('/blogs/new', function(req, res) {
+  res.render('new');
+});
+// CREATE ROUTE
+app.post('/blogs', function(req, res) {
+  // create blog
+  Blog.create(req.body.blog, function(err, newBlog) {
+    if (err) {
+      console.log(err);
+      res.render('new');
+    } else {
+      // then, redirect to the index
+      res.redirect('/blogs');
     }
   });
 });
