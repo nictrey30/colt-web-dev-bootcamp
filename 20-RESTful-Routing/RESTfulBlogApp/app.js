2f4e5ba965e99eb5cf7929c25eb970be9858dad2
Traian <niculescu.traian.ct@gmail.com>
2020-03-11 01:33:14 +0200

    finishing touches on RESTful blog

--- 20-RESTful-Routing/RESTfulBlogApp/app.js
+++ 20-RESTful-Routing/RESTfulBlogApp/app.js
@@ -1,9 +1,10 @@
 const express = require('express'),
   app = express(),
-  bodyParser = require('body-parser'),
-  mongoose = require('mongoose'),
-  methodOverride = require('method-override'),
-  port = 4000;
+  expressSanitizer = require('express-sanitizer');
+(bodyParser = require('body-parser')),
+  (mongoose = require('mongoose')),
+  (methodOverride = require('method-override')),
+  (port = 4000);
 // APP config
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
@@ -11,6 +12,7 @@ mongoose.set('useFindAndModify', false);
 mongoose.connect('mongodb://localhost:27017/restful_blog_app');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(expressSanitizer()); // expressSanitizer must be after bodyParser
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 // Moongose/Model config
@@ -42,6 +44,7 @@ app.get('/blogs/new', function(req, res) {
 // CREATE ROUTE
 app.post('/blogs', function(req, res) {
   // create blog
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.create(req.body.blog, function(err, newBlog) {
     if (err) {
       console.log(err);
@@ -76,6 +79,7 @@ app.get('/blogs/:id/edit', function(req, res) {
 // UPDATE ROUTE
 app.put('/blogs/:id', function(req, res) {
   // Blog.findByIdAndUpdate(id, newData, callback);
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(
     err,
     updatedBlog
