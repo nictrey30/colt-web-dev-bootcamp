08e28dd9578eb73fd365bab02d7526326c2696de
traian <traian30y@yahoo.co.uk>
2020-04-03 15:40:18 +0300

    update

--- 22-Authentication/app.js
+++ 22-Authentication/app.js
@@ -1,31 +1,42 @@
-const express = require('express'),
+const express = require("express"),
   app = express(),
-  mongoose = require('mongoose'),
-  passport = require('passport'),
-  LocalStrategy = require('passport-local'),
-  passportLocalMongoose = require('passport-local-mongoose'),
-  User = require('./models/user');
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose"),
+  User = require("./models/user");
 
 let port = 4000;
 
 // deprecation handlers mongoose
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 // mongoose connection
-mongoose.connect('mongodb://localhost:27017/auth-demo');
+mongoose.connect("mongodb://localhost:27017/auth-demo");
 
+// set app to use express-session
+app.use(
+  require("express-session")({
+    secret: "Ana are 5 mere !!",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 // app config
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+// tell app to use passport module
 app.use(passport.initialize());
 app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.get('/', function(req, res) {
-  res.render('home');
+app.get("/", function(req, res) {
+  res.render("home");
 });
-app.get('/secret', function(req, res) {
-  res.render('secret');
+app.get("/secret", function(req, res) {
+  res.render("secret");
 });
 
 app.listen(port, () => console.log(`Started server on port ${port}`));
--- 22-Authentication/models/user.js
+++ 22-Authentication/models/user.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose'),
-  passportLocalMongoose = require('passport-local-mongoose');
+const mongoose = require("mongoose"),
+  passportLocalMongoose = require("passport-local-mongoose");
 let UserSchema = new mongoose.Schema({
   user: String,
   password: String
 });
-
+// telling User to use serialize and deserialize methods of passportLocalMongoose
 UserSchema.plugin(passportLocalMongoose);
-module.exports = new mongoose.model('User', UserSchema);
+module.exports = new mongoose.model("User", UserSchema);
