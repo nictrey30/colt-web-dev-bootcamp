b24f5ca56636e9471b4cef3eb9c995ad80a77d0b
Traian <niculescu.traian.ct@gmail.com>
2020-04-24 10:12:34 +0300

    update from linux mint

--- 18-express-inter/EJSDemo/app.js
+++ 18-express-inter/EJSDemo/app.js
@@ -1,22 +1,23 @@
-let express = require('express');
-let app = express();
-const port = 3000;
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 // serve the contents of the 'public' folder
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 // to tell render() the file to render is .ejs
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   // res.send('<h1>Welcome to the home page!<h1>');
   res.render('home');
 });
-app.get('/fallinlovewith/:thing', function(req, res) {
+app.get('/fallinlovewith/:thing', function (req, res) {
   let thing = req.params.thing;
   // res.send(`You fell in love with ${thing}`);
   res.render('love', { thingVar: thing });
 });
-app.get('/posts', function(req, res) {
+app.get('/posts', function (req, res) {
   let posts = [
     { title: 'Post 1', author: 'Susy' },
     { title: 'Post 2', author: 'Charlie' },
@@ -24,4 +25,4 @@ app.get('/posts', function(req, res) {
   ];
   res.render('posts', { posts: posts });
 });
-app.listen(port, () => console.log(`server started on port ${port}`));
+app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
--- 18-express-inter/PostRequestDemo/app.js
+++ 18-express-inter/PostRequestDemo/app.js
@@ -1,21 +1,21 @@
-let express = require('express');
-let app = express();
-let bodyParser = require('body-parser');
-let port = 3000;
-let friends = ['Tony', 'Miranda', 'Justin', 'Pierre', 'Lily'];
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+const friends = ['Tony', 'Miranda', 'Justin', 'Pierre'];
 // tell express to use body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('home');
 });
 
-app.get('/friends', function(req, res) {
+app.get('/friends', function (req, res) {
   res.render('friends', { friends: friends });
 });
 
-app.post('/addFriend', function(req, res) {
+app.post('/addFriend', function (req, res) {
   // req.body is an obj that will contain all of the data from the request body,
   // after is parsed by express using body-parser package
   // console.log(req.body);
@@ -24,4 +24,4 @@ app.post('/addFriend', function(req, res) {
   res.redirect('/friends');
 });
 
-app.listen(port, () => console.log(`server started on port ${port}`));
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
